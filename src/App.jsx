72a61c29580be34913bdb1b9ed1520da786660f5
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-10 15:13:48 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { Route, BrowserRouter as Router, Routes  } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
-  return (
-    <main className=" bg-slate-300/20">
-    <Router>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={"Home"} />
-                  <Route path="/about" element={"about"} />
-            <Route path="/project" element={"project"} />
-                  <Route path="/contact" element={"contact"} />
-        </Routes>
-    </Router>
+    return (
+        <main className=" bg-slate-300/20">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </Router>
 
-    </main>
-  )
+        </main>
+    )
 }
 
 export default App
