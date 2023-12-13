3c222e076a3f37e306566c91ad78ddc8906ec57b
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-13 07:36:39 +0530

    homeinfo added for current stage 1 and 2

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import arrow from '../assets/icons/arrow.svg';
 
 const InfoBox = ({ text, link, btnText }) => (
     <div className="info-box">
-        {text}
-        <Link to={link}>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className='neo-brutalism-white neo-btn'>
             {btnText}
+            <img src={arrow} alt="arrow" />
         </Link>
     </div>
 )
 const renderContent = {
     1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>Hi, I am <span className='font-semibold'>Ramanand Thakur</span></h1>
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+            Hi, I am <span className='font-semibold'>Ramanand Thakur</span>👋<br />
+            A Software Engineer from India
+
+        </h1>
     ),
     2: (
-        <InfoBox />
+        <InfoBox 
+            text={"As a MERN stack expert and passionate React Native enthusiast, 🌟 I've brought enchantment to various projects, crafting tech wonders for companies and individuals alike! ✨"}
+            link={"/about"}
+            btnText={'Learn more'}
+        />
     ),
     3: (
         <h1>3</h1>
