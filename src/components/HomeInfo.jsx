33677c8b4bfd231114253d90e8c5498e00fbadb9
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-13 07:13:02 +0530

    info box added

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const renderContent ={
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        {text}
+        <Link to={link}>
+            {btnText}
+        </Link>
+    </div>
+)
+const renderContent = {
     1: (
-        <h1>1</h1>
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>Hi, I am <span className='font-semibold'>Ramanand Thakur</span></h1>
     ),
     2: (
-        <h1>2</h1>
+        <InfoBox />
     ),
     3: (
         <h1>3</h1>
@@ -14,9 +23,9 @@ const renderContent ={
         <h1>4</h1>
     ),
 }
-const HomeInfo = ({currentStage}) => {
-  return renderContent[currentStage] || null;
-  
+const HomeInfo = ({ currentStage }) => {
+    return renderContent[currentStage] || null;
+
 }
 
 export default HomeInfo
