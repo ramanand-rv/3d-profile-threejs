7c24489108e5339d77aa5d93104e2be9927fbb1b
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-13 06:49:29 +0530

    home info rendered on current stages only

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeInfo = () => {
+  return (
+    <div>Pop up</div>
+  )
+}
+
+export default HomeInfo
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Island from "../models/Island"; ''
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
+import HomeInfo from "../components/HomeInfo";
 
 const Home = () => {
   const [currentStage, setCurrentStage] = useState(1);
@@ -37,9 +38,9 @@ const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
   return (
     <section className="w-full h-screen relative">
-      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        Pop Up
-      </div> */}
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
 
       <Canvas
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
