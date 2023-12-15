d1df2725e85c808c7a398e8262b009e6c0056e4d
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-15 23:54:56 +0530

    social links updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,12 +173,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/ramanand-rv',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/ramanand-rv/',
     }
 ];
 
